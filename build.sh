5c738368d0b292da24355b12ebcec3874339bf2d
hannes <hannes@pop-os.localdomain>
2023-12-10 11:02:38 +0100

    add linux build

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+peak="../peak"
+opt="-Wall -Wextra -std=c++20 -msse4.1 $@"
+inc="-I$peak/src"
+lib="-pthread -lxcb -lxcb-xkb -lxkbcommon -lxkbcommon-x11 -lvulkan -lasound $peak/src/lib.cpp"
+out="out"
+
+mkdir -p $out
+
+g++ main.cpp -o $out/$1 $opt $inc $lib
